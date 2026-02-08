071cd73c370503b43864ee34e75202cf50303795
Nitin Thakur <ntt45419@gmail.com>
2026-02-09 05:15:57 +0530

    Added sample routes and component folder

---
+++ src/app/layout.jsx
@@ -0,0 +1,19 @@
+import { Logo, Navigation } from "@/components";
+
+export const metadata = {
+  title: "The wild oasis",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <header>
+          <Logo />
+          <Navigation />
+        </header>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>The Wild Oasis. Welcome to Paradise</h1>
+    </main>
+  )
+}
