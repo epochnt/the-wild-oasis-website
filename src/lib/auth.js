4d4f6a3ba3b529146c6c023980fc2b07a384e45c
Nitin Thakur <ntt45419@gmail.com>
2026-02-16 22:46:43 +0530

    ䷍ Session added

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth'
 import Google from 'next-auth/providers/google'
+import { createGuest, getGuest } from '@/lib/data-service'
 
 const authConfig = {
   providers: [
@@ -14,6 +15,24 @@ const authConfig = {
       // if (!authorized)
       //   return Response.redirect(new URL('/api/auth/signin', request.url))
       return !!auth?.user
+    },
+    //sign in runs after the success for signIn but before the session has started (middleware middleware)
+    async signIn({ user, account, profile }) {
+      console.log(user, profile, account)
+      try {
+        const guest = await getGuest(user.email)
+        if (!guest)
+          await createGuest({ email: user.email, fullName: user.name })
+        return true
+      } catch (error) {
+        return false
+      }
+    },
+
+    async session({ session }) {
+      const guest = await getGuest(session.user.email)
+      session.user.guestId = guest.id
+      return session
     },
   },
   pages: {
